8bf45fa746e0aefffdef9e233d715e37f89f12c0
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-07 19:25:05 +0800

    add some other commit content

--- src/Components/Expenseltem.js
+++ src/Components/Expenseltem.js
@@ -1,6 +1,15 @@
 // a react component is just a js function
 const ExpenseItem = () => {
-  return <h2>Expense item!</h2>;
+  return (
+    // only have one root element
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
