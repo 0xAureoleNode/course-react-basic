e9f55868de42555c56c9572f3657a35dac073bd0
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-07 21:20:53 +0800

    let the component reuseable

--- src/Components/Expenseltem.js
+++ src/Components/Expenseltem.js
@@ -1,18 +1,15 @@
 import './ExpenseItem.css';
 // a react component is just a js function
-const ExpenseItem = () => {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
+const ExpenseItem = ({ title, amount, date }) => {
   return (
     // only have one root element
-    <di v className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+    <div className="expense-item">
+      <div>{date.toISOString}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
-    </di>
+    </div>
   );
 };
 
