621f486454e314f952dc9ad06de5edc6a5171699
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-10 15:04:55 +0800

    handling form submission

--- src/Components/ExpenseForm/ExpenseForm.js
+++ src/Components/ExpenseForm/ExpenseForm.js
@@ -33,8 +33,18 @@ const ExpenseForm = () => {
     setEnteredDate(e.target.value);
   };
 
+  const submitHandle = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandle}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
