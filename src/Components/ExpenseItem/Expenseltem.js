2aef315d90138e756f8d832a2d978560add1e3fd
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-10 12:11:46 +0800

    update state by useState

--- src/Components/ExpenseItem/Expenseltem.js
+++ src/Components/ExpenseItem/Expenseltem.js
@@ -1,17 +1,22 @@
 import './ExpenseItem.css';
 import ExpenseDate from '../ExpenseDate/ExpenseDate';
 import Card from '../Card/Card';
+import { useState } from 'react';
 // a react component is just a js function
 const ExpenseItem = ({ date, title, amount }) => {
-  // const { date, title, amount } = props;
+  const [itemTitle, setItemTitle] = useState(title);
+  const clickHandler = () => {
+    setItemTitle('updated!');
+  };
   return (
     // only have one root element
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{itemTitle}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
