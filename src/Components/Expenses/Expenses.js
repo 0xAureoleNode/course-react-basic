94a3bfa36002f329b52923c458c286c140439f8c
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-10 22:35:05 +0800

    add ExpensesFilter Event & State

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,19 +1,32 @@
 import ExpenseItem from '../ExpenseItem/Expenseltem';
+import ExpensesFilter from '../ExpensesFilter/ExpensesFilter';
 import './Expenses.css';
 import Card from '../Card/Card';
+import { useState } from 'react';
 const Expenses = ({ expenses }) => {
+  const [selectOption, setSelectOption] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setSelectOption(selectedYear);
+  };
   return (
-    <Card className="expenses">
-      {expenses.map((expense) => (
-        <div key={expense.id}>
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        </div>
-      ))}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          onChangeFilter={filterChangeHandler}
+          value={selectOption}
+        />
+        {expenses.map((expense) => (
+          <div key={expense.id}>
+            <ExpenseItem
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          </div>
+        ))}
+      </Card>
+    </div>
   );
 };
 
