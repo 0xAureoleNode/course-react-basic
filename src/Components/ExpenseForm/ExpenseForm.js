d823d721333c2b5b054d13e454641e29864822ff
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-10 13:50:21 +0800

    update three state

--- src/Components/ExpenseForm/ExpenseForm.js
+++ src/Components/ExpenseForm/ExpenseForm.js
@@ -1,19 +1,51 @@
+import { useState } from 'react';
 import './ExpenseForm.css';
+
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  const titleChangeHandle = (e) => {
+    setEnteredTitle(e.target.value);
+  };
+
+  const amountChangeHandle = (e) => {
+    setEnteredAmount(e.target.value);
+  };
+
+  const dateChangeHandle = (e) => {
+    setEnteredDate(e.target.value);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={titleChangeHandle}
+            value={enteredTitle}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandle}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandle}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
