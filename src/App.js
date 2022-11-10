1d428d6c64726826e954e9902a255b04e488d090
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-10 13:20:07 +0800

    add ExpenseForm & NewExpense

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './Components/Expenses/Expenses';
+import NewExpense from './Components/NewExpense/NewExpense';
 function App() {
   const expenses = [
     {
@@ -28,6 +29,7 @@ function App() {
   ];
   return (
     <div>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
